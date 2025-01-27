9b47bd3d8317c7e6cfe996993e33b3a244eb052c
laserchicken <mateusz.kmiecinski@yahoo.com>
2025-01-27 12:36:57 +0100

    add recursive 2.27 solution

--- 2.27.rkt
+++ 2.27.rkt
@@ -13,3 +13,16 @@
 (deep-reverse (list 1 2 (list 3 4)))
 (deep-reverse (list (list 0 1) 2 (list 3 4)))
 (deep-reverse (list (list 0 1) 2 3 4))
+
+(define (deep-rev list)
+  (cond ((null? list) nil)
+        ((not (pair? (car list)))
+         (cons (car list) (deep-rev (cdr list))))
+        (else
+         (cons (deep-rev (car list)) (deep-rev (cdr list))))))
+
+(newline)
+(deep-reverse (list (list 1 2) (list 3 4)))
+(deep-reverse (list 1 2 (list 3 4)))
+(deep-reverse (list (list 0 1) 2 (list 3 4)))
+(deep-reverse (list (list 0 1) 2 3 4))
