56cf6899128a4e5686096074a50b26da5551a1f5
laserchicken <mateusz.kmiecinski@yahoo.com>
2025-01-27 12:45:12 +0100

    refactor 2.27 solution

--- 2.27.rkt
+++ 2.27.rkt
@@ -1,25 +1,25 @@
 #lang sicp
 
-(define (deep-reverse list)
-  (define (deep-reverse-iter list rev)
-    (cond ((null? list) rev)
-          ((not (pair? (car list)))
-           (deep-reverse-iter (cdr list) (cons (car list) rev)))
+(define (deep-reverse tree)
+  (define (deep-reverse-iter tree rev)
+    (cond ((null? tree) rev)
+          ((not (pair? (car tree)))
+           (deep-reverse-iter (cdr tree) (cons (car tree) rev)))
           (else
-           (deep-reverse-iter (cdr list) (cons (deep-reverse (car list)) rev)))))
-  (deep-reverse-iter list nil))
+           (deep-reverse-iter (cdr tree) (cons (deep-reverse (car tree)) rev)))))
+  (deep-reverse-iter tree nil))
 
 (deep-reverse (list (list 1 2) (list 3 4)))
 (deep-reverse (list 1 2 (list 3 4)))
 (deep-reverse (list (list 0 1) 2 (list 3 4)))
 (deep-reverse (list (list 0 1) 2 3 4))
 
-(define (deep-rev list)
-  (cond ((null? list) nil)
-        ((not (pair? (car list)))
-         (cons (car list) (deep-rev (cdr list))))
+(define (deep-rev tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree))
+         (list tree))
         (else
-         (cons (deep-rev (car list)) (deep-rev (cdr list))))))
+         (cons (deep-rev (car tree)) (deep-rev (cdr tree))))))
 
 (newline)
 (deep-reverse (list (list 1 2) (list 3 4)))
