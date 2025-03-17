946978f42686be5361e239439cef5dba32c51eb1
laserchicken <mateusz.kmiecinski@yahoo.com>
2025-03-17 23:02:08 +0100

    add 2.50 solution

---
+++ 2.50.rkt
@@ -0,0 +1,136 @@
+#lang racket
+(require graphics/graphics)
+(open-graphics)
+(define vp (open-viewport "A Picture Language" 500 500))
+
+(define draw (draw-viewport vp))
+(define (clear) ((clear-viewport vp)))
+(define line (draw-line vp))
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (car (cdr frame)))
+
+(define (edge2-frame frame)
+  (car (cdr (cdr frame))))
+
+(define (make-frame-2 origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame-2 frame)
+  (car frame))
+
+(define (edge1-frame-2 frame)
+  (car (cdr frame)))
+
+(define (edge2-frame-2 frame)
+  (cdr (cdr frame)))
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+             (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+             (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect s v)
+  (make-vect (* (xcor-vect v) s) (* (ycor-vect v) s)))
+
+(define (make-segment start-v end-v)
+  (cons start-v end-v))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
+               (scale-vect (ycor-vect v) (edge2-frame frame))))))
+
+(define (vector-to-posn v)
+  (make-posn (xcor-vect v) (ycor-vect v)))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (line
+        (vector-to-posn ((frame-coord-map frame)
+         (start-segment segment)))
+        (vector-to-posn ((frame-coord-map frame)
+         (end-segment segment)))))
+     segment-list)))
+
+
+(define outline-painter
+  (segments->painter (list (make-segment (make-vect 0 0) (make-vect 0 1))
+                           (make-segment (make-vect 0 1) (make-vect 1 1))
+                           (make-segment (make-vect 1 1) (make-vect 1 0))
+                           (make-segment (make-vect 1 0) (make-vect 0 0)))))
+
+(define x-painter
+  (segments->painter (list (make-segment (make-vect 0 0) (make-vect 1 1))
+                           (make-segment (make-vect 0 1) (make-vect 1 0)))))
+
+(define diamond-painter
+  (segments->painter (list (make-segment (make-vect 0 0.5) (make-vect 0.5 1))
+                           (make-segment (make-vect 0.5 1) (make-vect 1 0.5))
+                           (make-segment (make-vect 1 0.5) (make-vect 0.5 0))
+                           (make-segment (make-vect 0.5 0) (make-vect 0 0.5)))))
+
+(define diagonal-painter
+  (segments->painter (list (make-segment (make-vect 0 0) (make-vect 1 1)))))
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame
+                  new-origin
+                  (sub-vect (m corner1) new-origin)
+                  (sub-vect (m corner2) new-origin)))))))
+
+(define (flip-horizont painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0) ; new origin
+                     (make-vect 1.0 1.0) ; new end of edge1
+                     (make-vect 0.0 0.0))) ; new end of edge2
+
+(define (rotate-cc-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0) ; new origin
+                     (make-vect 0.0 1.0) ; new end of edge1
+                     (make-vect 1.0 0.0))) ; new end of edge2
+
+(define (rotate-cc-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0) ; new origin
+                     (make-vect 0.0 0.0) ; new end of edge1
+                     (make-vect 1.0 1.0))) ; new end of edge2
+
+;((flip-horizont diagonal-painter) (make-frame (make-vect 100 100) (make-vect 0 300) (make-vect 300 0)))
+
+;((rotate-cc-180 diagonal-painter) (make-frame (make-vect 100 100) (make-vect 0 300) (make-vect 300 0)))
+
+((rotate-cc-270 diagonal-painter) (make-frame (make-vect 100 100) (make-vect 0 300) (make-vect 300 0)))
